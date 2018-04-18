262f97f6324196f120e9d02536a93ec5e2d00394
xX_andrescol_Xx <andresmil24@hotmail.com>
2018-04-18 18:23:43 -0500

    Inicie BaseDAL, clase base para el accedo a MongoDB

---
+++ VCI_back/VCI_API/VCI_API/Models/DAL/BaseDAL.cs
@@ -0,0 +1,43 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using VCI_API.Models.DTO;
+
+namespace VCI_API.Models.DAL
+{
+    public class BaseDAL<T,D> where T : BaseDTO<D>
+    {
+        private static IMongoClient Client;
+        private IMongoDatabase Database;
+        private IMongoCollection<T> Collection;
+
+        private string DatabaseName;
+        private string CollectionName;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="CollectionName">Nombre de la colección</param>
+        public BaseDAL(string CollectionName)
+        {
+            DatabaseName = ConfigurationManager.AppSettings["DbName"];
+            this.CollectionName = CollectionName;
+
+            if (Client == null)
+            {
+                Client = new MongoClient(ConfigurationManager.AppSettings["DbEndpoint"]);
+            }
+            Database = Client.GetDatabase(DatabaseName);
+            Collection = Database.GetCollection<T>(CollectionName);
+        }
+
+        public T GetById(D id)
+        {
+
+        }
+
+    }
+}
---
+++ VCI_back/VCI_API/VCI_API/Models/DTO/BaseDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace VCI_API.Models.DTO
+{
+    public class BaseDTO<T>
+    {
+        public T Id { get; set; }
+
+    }
+}
